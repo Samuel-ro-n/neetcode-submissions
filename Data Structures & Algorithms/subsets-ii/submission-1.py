8bb555b5882eb42dc46b433ae21d12c05619638d
Samuel-ro-n <82696277+Samuel-ro-n@users.noreply.github.com>
2026-06-24 21:04:00 +0100

    Add: subsets-ii - submission-1

---
+++ Data Structures & Algorithms/subsets-ii/submission-1.py
@@ -0,0 +1,16 @@
+class Solution:
+    def subsetsWithDup(self, nums: List[int]) -> List[List[int]]:
+        Newset=[[]]
+        for num in nums:
+            subset=[]
+            for i in Newset:
+                Value= i + [num]
+                subset.append(Value)
+                subset.sort
+            Newset += subset
+        unique=[]
+        for subset in Newset:
+            subset.sort()
+            if subset not in unique:
+                unique.append(subset)
+        return unique
