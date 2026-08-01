191be2a75df76ceda16b3c0b7de672624d1566cb
Samuel-ro-n <82696277+Samuel-ro-n@users.noreply.github.com>
2026-08-01 11:51:52 +0100

    Add: daily-temperatures - submission-9

---
+++ Data Structures & Algorithms/daily-temperatures/submission-9.py
@@ -0,0 +1,30 @@
+class Solution:
+    def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
+        stack=[]
+        n= len(temperatures)
+        if not temperatures:
+            return 0
+        l, r= 0, 1
+        while l < (n-1):
+            if r== n:
+                stack.append(0)
+                l+=1
+                r= l+1
+            elif temperatures[l] < temperatures[r]:
+                stack.append(r-l)
+                l+=1
+                r= l+1
+            else:
+                r+=1
+        stack.append(0)
+        return stack
+    
+
+
+
+        
+
+
+
+
+        
