78afe89eebc4e7899b8a60004f0dff223ff2f31f
Samuel-ro-n <82696277+Samuel-ro-n@users.noreply.github.com>
2026-08-03 12:53:55 +0100

    Delete Data Structures & Algorithms/anagram-groups/submission-2.py

--- Data Structures & Algorithms/anagram-groups/submission-2.py
+++
@@ -1,9 +0,0 @@
-class Solution:
-    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        wordict= {}
-        for word in strs:
-            sw= ''.join.sorted(word)
-            if sw not in wordict:
-                wordict[sw]== []
-            wordict[sw].append(word)
-        return wordict.values
